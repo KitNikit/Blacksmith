e911818e4f2e06ba35a7b894c3381e38e94f236f
Nikita <vclan@mail.ru>
2021-07-29 20:08:58 +0300

    crossorigin

--- pages/_document.js
+++ pages/_document.js
@@ -7,11 +7,7 @@ class MyDocument extends Document {
 				<Head>
 					<link rel='shortcut icon' href='favicon.png' type='image/png'></link>
 					<link rel='preconnect' href='https://fonts.googleapis.com'></link>
-					<link
-						rel='preconnect'
-						href='https://fonts.gstatic.com'
-						crossOrigin
-					></link>
+					<link rel='preconnect' href='https://fonts.gstatic.com'></link>
 					<link
 						href='https://fonts.googleapis.com/css2?family=Roboto&display=swap'
 						rel='stylesheet'
